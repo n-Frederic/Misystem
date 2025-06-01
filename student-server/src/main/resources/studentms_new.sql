a8dbdfa744c1951d723b268f521b93479f93a921
n-Frederic <xuechaozhou001@gmail.com>
2025-06-01 17:54:11 +0800

    对sql语句作了些调整
    加入了星期几以及period -时间段，方便后期引入Timetable

--- student-server/src/main/resources/studentms_new.sql
+++ student-server/src/main/resources/studentms_new.sql
@@ -25,21 +25,23 @@ CREATE TABLE `c`  (
   `cid` int NOT NULL AUTO_INCREMENT,
   `cname` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `ccredit` tinyint NULL DEFAULT NULL,
+  `day` int not null,
+  `period` int not null,
   PRIMARY KEY (`cid`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of c
 -- ----------------------------
-INSERT INTO `c` VALUES (7, '数据结构', 3);
-INSERT INTO `c` VALUES (8, '数据库系统', 3);
-INSERT INTO `c` VALUES (9, '计算机网络', 3);
-INSERT INTO `c` VALUES (10, '计算机组成原理', 3);
-INSERT INTO `c` VALUES (11, '软件工程导论', 1);
-INSERT INTO `c` VALUES (12, '离散数学', 3);
-INSERT INTO `c` VALUES (13, '人工智能基础', 2);
-INSERT INTO `c` VALUES (14, '程序设计基础', 3);
-INSERT INTO `c` VALUES (15, '大数据概论', 2);
+INSERT INTO `c` VALUES (7, '数据结构', 3, 1, 1);
+INSERT INTO `c` VALUES (8, '数据库系统', 3, 1, 4);
+INSERT INTO `c` VALUES (9, '计算机网络', 3, 1, 5);
+INSERT INTO `c` VALUES (10, '计算机组成原理', 3, 1, 6);
+INSERT INTO `c` VALUES (11, '软件工程导论', 1, 1, 7);
+INSERT INTO `c` VALUES (12, '离散数学', 3, 2, 1);
+INSERT INTO `c` VALUES (13, '人工智能基础', 2, 2, 2);
+INSERT INTO `c` VALUES (14, '程序设计基础', 3, 2, 4);
+INSERT INTO `c` VALUES (15, '大数据概论', 2, 2, 6);
 
 -- ----------------------------
 -- Table structure for ct
@@ -60,12 +62,12 @@ CREATE TABLE `ct`  (
 -- ----------------------------
 -- Records of ct
 -- ----------------------------
-INSERT INTO `ct` VALUES (5, 8, 4, '25-春季学期');
-INSERT INTO `ct` VALUES (6, 7, 4, '25-春季学期');
-INSERT INTO `ct` VALUES (7, 10, 13, '25-春季学期');
-INSERT INTO `ct` VALUES (8, 9, 13, '25-春季学期');
-INSERT INTO `ct` VALUES (9, 11, 4, '25-春季学期');
-INSERT INTO `ct` VALUES (10, 9, 4, '25-秋季学期');
+INSERT INTO `ct` VALUES (1, 8, 4, '25-春季学期');
+INSERT INTO `ct` VALUES (2, 7, 4, '25-春季学期');
+INSERT INTO `ct` VALUES (3, 10, 13, '25-春季学期');
+INSERT INTO `ct` VALUES (4, 9, 13, '25-春季学期');
+INSERT INTO `ct` VALUES (5, 11, 4, '25-春季学期');
+INSERT INTO `ct` VALUES (6, 9, 4, '25-秋季学期');
 
 -- ----------------------------
 -- Table structure for s
@@ -81,14 +83,14 @@ CREATE TABLE `s`  (
 -- ----------------------------
 -- Records of s
 -- ----------------------------
-INSERT INTO `s` VALUES (1, '张三', '123');
-INSERT INTO `s` VALUES (2, '李四', '123456');
-INSERT INTO `s` VALUES (3, '王五', 'pass01');
-INSERT INTO `s` VALUES (4, '赵六', '111111');
-INSERT INTO `s` VALUES (5, '陈七', 'root01');
-INSERT INTO `s` VALUES (6, '刘八', 'java22');
-INSERT INTO `s` VALUES (7, '杨九', 'cstart');
-INSERT INTO `s` VALUES (8, '吴十', 'sqlpwd');
+INSERT INTO `s` VALUES (1, '周学超', '123');
+INSERT INTO `s` VALUES (2, '姜雨彤', '123456');
+INSERT INTO `s` VALUES (3, '韦雪', 'pass01');
+INSERT INTO `s` VALUES (4, '黄爱雷', '111111');
+INSERT INTO `s` VALUES (5, '贾佩伦', 'root01');
+INSERT INTO `s` VALUES (6, '薛探昕', 'java22');
+INSERT INTO `s` VALUES (7, '胡佳乐', 'cstart');
+INSERT INTO `s` VALUES (8, '郭开蒙', 'sqlpwd');
 INSERT INTO `s` VALUES (9, '周十一', 'math01');
 INSERT INTO `s` VALUES (10, '郑十二', 'vue123');
 
