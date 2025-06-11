debd633bd4c4259626081caa1f954bee45af7e3a
05hal <1831437770@qq.com>
2025-06-11 11:37:46 +0800

    exam后端改正

--- student-server/src/main/java/com/auggie/student_server/entity/Exam.java
+++ student-server/src/main/java/com/auggie/student_server/entity/Exam.java
@@ -11,6 +11,7 @@ import org.apache.ibatis.type.Alias;
 @Alias("Exam")
 public class Exam {
     private Integer cno;
+    private String cname;
     private Date day;
     private Integer period;
     private Integer durationMin;
@@ -18,4 +19,67 @@ public class Exam {
     private String location;
     private String Req;
 
+    public Integer getCno() {
+        return cno;
+    }
+
+    public void setCno(Integer cno) {
+        this.cno = cno;
+    }
+
+    public String getCname() {
+        return cname;
+    }
+
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+
+    public Date getDay() {
+        return day;
+    }
+
+    public void setDay(Date day) {
+        this.day = day;
+    }
+
+    public Integer getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(Integer period) {
+        this.period = period;
+    }
+
+    public Integer getDurationMin() {
+        return durationMin;
+    }
+
+    public void setDurationMin(Integer durationMin) {
+        this.durationMin = durationMin;
+    }
+
+    public Float getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Float grade) {
+        this.grade = grade;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getReq() {
+        return Req;
+    }
+
+    public void setReq(String req) {
+        Req = req;
+    }
 }
