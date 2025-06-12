cd45c337f5eb84fd522e28e84ffe19ce915e7e91
05hal <1831437770@qq.com>
2025-06-12 22:52:47 +0800

    合并admin的课程管理和开课表管理

--- student-client/src/views/Admin/courseManage/queryCourse.vue
+++ student-client/src/views/Admin/courseManage/queryCourse.vue
@@ -1,68 +1,91 @@
 <template>
-  <div>
-    <el-container>
-      <el-main>
-        <el-card>
-          <el-form :inline="true" :model="ruleForm" :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
-            <el-form-item label="课程号" prop="cno">
-              <el-input v-model.number="ruleForm.cno"></el-input>
-            </el-form-item>
-            <el-form-item label="课程名" prop="cname">
-              <el-input v-model="ruleForm.cname"></el-input>
-            </el-form-item>
-            <el-form-item label="模糊查询" prop="fuzzy">
-              <el-switch v-model="ruleForm.fuzzy"></el-switch>
-            </el-form-item>
-            <el-form-item label="学分下限" prop="lowBound">
-              <el-input v-model.number="ruleForm.lowBound"></el-input>
-            </el-form-item>
-            <el-form-item label="学分上限" prop="highBound">
-              <el-input v-model.number="ruleForm.highBound"></el-input>
-            </el-form-item>
-            <el-form-item>
-              <el-button type="primary" @click="resetForm('ruleForm')">重置</el-button>
-            </el-form-item>
-          </el-form>
-        </el-card>
-        <el-card>
-          <course-list :ruleForm="ruleForm" :isActive="true"></course-list>
-        </el-card>
-      </el-main>
-    </el-container>
-  </div>
+    <div>
+        <el-container>
+            <el-main>
+                <el-card>
+                    <el-form :inline="true" :model="ruleForm" :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
+                        <!-- 课程查询字段 -->
+                        <el-form-item label="课程号" prop="cno">
+                            <el-input v-model.number="ruleForm.cno"></el-input>
+                        </el-form-item>
+                        <el-form-item label="课程名" prop="cname">
+                            <el-input v-model="ruleForm.cname"></el-input>
+                        </el-form-item>
+                        <el-form-item label="课程模糊查询" prop="cFuzzy">
+                            <el-switch v-model="ruleForm.cFuzzy"></el-switch>
+                        </el-form-item>
+                        <el-form-item label="学分下限" prop="lowBound">
+                            <el-input v-model.number="ruleForm.lowBound"></el-input>
+                        </el-form-item>
+                        <el-form-item label="学分上限" prop="highBound">
+                            <el-input v-model.number="ruleForm.highBound"></el-input>
+                        </el-form-item>
+
+                        <!-- 新增：教师查询字段 -->
+                        <el-form-item label="工号" prop="tid">
+                            <el-input v-model.number="ruleForm.tid"></el-input>
+                        </el-form-item>
+                        <el-form-item label="教师名" prop="tname">
+                            <el-input v-model="ruleForm.tname"></el-input>
+                        </el-form-item>
+                        <el-form-item label="教师模糊查询" prop="tFuzzy">
+                            <el-switch v-model="ruleForm.tFuzzy"></el-switch>
+                        </el-form-item>
+
+                        <el-form-item>
+                            <el-button type="primary" @click="resetForm('ruleForm')">重置</el-button>
+                        </el-form-item>
+                    </el-form>
+                </el-card>
+                <el-card>
+                    <course-list :ruleForm="ruleForm" :isActive="true"></course-list>
+                </el-card>
+            </el-main>
+        </el-container>
+    </div>
 </template>
+
 <script>
 import CourseList from "@/views/Admin/courseManage/courseList";
+
 export default {
-  components: {CourseList},
-  data() {
-    return {
-      ruleForm: {
-        cno: null,
-        cname: null,
-        fuzzy: true,
-        lowBound: null,
-        highBound: null
-      },
-      rules: {
-        cno: [
-          { type: 'number', message: '必须是数字类型' }
-        ],
-        cname: [
-        ],
-        lowBound: [
-          { type: 'number', message: '必须是数字类型' }
-        ],
-        highBound: [
-          { type: 'number', message: '必须是数字类型' }
-        ],
-      }
-    };
-  },
-  methods: {
-    resetForm(formName) {
-      this.$refs[formName].resetFields();
+    components: { CourseList },
+    data() {
+        return {
+            ruleForm: {
+                // 原有的课程查询字段
+                cno: null,
+                cname: null,
+                cFuzzy: true,
+                lowBound: null,
+                highBound: null,
+
+                // 新增的教师查询字段
+                tid: null,
+                tname: null,
+                tFuzzy: true
+            },
+            rules: {
+                cno: [
+                    { type: 'number', message: '必须是数字类型' }
+                ],
+                lowBound: [
+                    { type: 'number', message: '必须是数字类型' }
+                ],
+                highBound: [
+                    { type: 'number', message: '必须是数字类型' }
+                ],
+                // 新增教师字段的验证规则
+                tid: [
+                    { type: 'number', message: '必须是数字类型' }
+                ]
+            }
+        };
+    },
+    methods: {
+        resetForm(formName) {
+            this.$refs[formName].resetFields();
+        }
     }
-  }
 }
 </script>
